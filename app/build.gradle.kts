4d09cd205ad762dfbdaa424628ea1704d7cc6bc0
tapendrabistao1 <tapendrabista01@gmail.com>
2024-10-10 19:22:05 +0545

    changed 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.tapsss"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.1.3"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
